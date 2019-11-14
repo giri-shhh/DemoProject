75d2cfa7289efc3dc5238abe610efaf53bee8b1d
Girish <pgirisha77@gmail.com>
2019-11-14 21:19:34 +0530

    Collectors function implementation

--- src/com/demo/test/Sample3.java
+++ src/com/demo/test/Sample3.java
@@ -1,12 +1,13 @@
 package com.demo.test;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Sample3 {
 
 	public static void main(String[] args) {
 		System.out
-				.println(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9).parallelStream().map(x -> x).collect(Collectors.toList()));
+				.println(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).parallelStream().map(x -> x)
+						.collect(Collectors.toList()));
 	}
 }
