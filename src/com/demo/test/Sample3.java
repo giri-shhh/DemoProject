61f22bfb52a8695082231e345bc7ed482e019248
Girish <pgirisha77@gmail.com>
2019-11-15 01:23:23 +0530

    Added hackerrank sample test

--- src/com/demo/test/Sample3.java
+++ src/com/demo/test/Sample3.java
@@ -1,13 +1,39 @@
 package com.demo.test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Sample3 {
 
 	public static void main(String[] args) {
-		System.out
-				.println(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).parallelStream().map(x -> x)
-						.collect(Collectors.toList()));
+		Scanner scanner = new Scanner(System.in);
+		double payment = scanner.nextDouble();
+		scanner.close();
+		NumberFormat usinstance = NumberFormat.getInstance(Locale.US);
+		usinstance.setMaximumFractionDigits(2);
+		usinstance.setMinimumFractionDigits(2);
+
+		NumberFormat ininstance = NumberFormat.getInstance(Locale.ENGLISH);
+		ininstance.setMaximumFractionDigits(2);
+		ininstance.setMinimumFractionDigits(2);
+		
+		NumberFormat chinaInstance = NumberFormat.getInstance(Locale.US);
+		chinaInstance.setMaximumFractionDigits(2);
+		chinaInstance.setMinimumFractionDigits(2);
+		
+		NumberFormat franceInstance = NumberFormat.getInstance(Locale.US);
+		franceInstance.setMaximumFractionDigits(2);
+		franceInstance.setMinimumFractionDigits(2);
+		
+		String us = "$" + usinstance.format(payment);
+		String india = "Rs." + ininstance.format(payment);
+		String china = "￥" + NumberFormat.getInstance(Locale.CHINA).format(payment);
+		String france = NumberFormat.getInstance(Locale.FRANCE).format(payment) + " €";
+
+		System.out.println("US: " + us);
+		System.out.println("India: " + india);
+		System.out.println("China: " + china);
+		System.out.println("France: " + france);
 	}
 }
