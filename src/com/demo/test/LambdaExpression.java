92c77270ed451c2e407b8e24d98ce26d35982a67
Girish <pgirisha77@gmail.com>
2019-11-30 14:45:26 +0530

    Added solution for can you access problem

---
+++ src/com/demo/test/LambdaExpression.java
@@ -0,0 +1,71 @@
+package com.demo.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+interface PerformOperation {
+	boolean check(int a);
+}
+
+class MyMath {
+	public static boolean checker(PerformOperation p, int num) {
+		return p.check(num);
+	}
+
+	public PerformOperation isOdd() {
+		return num -> num % 2 != 0;
+	}
+
+	public PerformOperation isPrime() {
+		return num -> IntStream.rangeClosed(1, num).filter(i -> num % i == 0).count() == 2;
+	}
+
+	public PerformOperation isPalindrome() {
+		return num -> {
+			int temp = num;
+			int r, sum = 0;
+			while (num > 0) {
+				r = num % 10; 
+				sum = (sum * 10) + r;
+				num = num / 10;
+			}
+			return temp == sum;
+		};
+	}
+}
+
+public class LambdaExpression {
+
+	public static void main(String[] args) throws IOException {
+		MyMath ob = new MyMath();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		PerformOperation op;
+		boolean ret = false;
+		String ans = null;
+		while (T-- > 0) {
+			String s = br.readLine().trim();
+			StringTokenizer st = new StringTokenizer(s);
+			int ch = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			if (ch == 1) {
+				op = ob.isOdd();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "ODD" : "EVEN";
+			} else if (ch == 2) {
+				op = ob.isPrime();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "PRIME" : "COMPOSITE";
+			} else if (ch == 3) {
+				op = ob.isPalindrome();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+			}
+			System.out.println(ans);
+		}
+	}
+}
